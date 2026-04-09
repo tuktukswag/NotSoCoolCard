401ef4497e49e7e31094a7b2a5be9a74d69f41b8
tuktukswag <109484200+tuktukswag@users.noreply.github.com>
2026-04-09 15:30:33 +0200

    a

--- app.py
+++ app.py
@@ -394,5 +394,5 @@ def api_deck_resolve():
 
 ## Main entry point to run the Flask app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
